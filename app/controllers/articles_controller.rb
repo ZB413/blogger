fe0302fffaf893dec697eba4ce4a4c1e7a983c65
Zach <ZachBelton413@gmail.com>
2017-03-26 14:09:17 -0500

    Working features - helper broken

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,35 @@
 class ArticlesController < ApplicationController
+  # ArticlesHelper - articles_helper.rb module not working for some reason
   def index
     @articles = Article.all
   end
   def show
     @article = Article.find(params[:id])
+  end
+  def new
+    @article = Article.new
+  end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+  def destroy
+    @article = Article.destroy(params[:id])
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  # article_params supposed to be in articles_helper
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
